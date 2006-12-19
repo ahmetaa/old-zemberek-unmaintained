16cd39f4db0569e3524b0f9352addd4943dcdc6b
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2006-12-19 17:34:33 +0000

    Dbus arayüzü ile ilgili düzenlemeler

--- zemberek-server/runtest.sh
+++ zemberek-server/runtest.sh
@@ -1,1 +1,3 @@
-java -cp ./zemberek_server-0.6.jar net.zemberekserver.client.TestClient
+#!/bin/sh
+java -cp .:lib/dbus.jar:zemberek_server-0.6.jar -Djava.library.path=lib net.zemberekserver.client.TestClient
+java -cp .:lib/dbus.jar:zemberek_server-0.6.jar -Djava.library.path=lib net.zemberekserver.client.TestDBus
