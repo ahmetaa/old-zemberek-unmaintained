6182293e5f14ee7e49630bdcd70c9cc4d73ee555
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-01-16 00:47:05 +0000

    Issue #25 Tekrar

--- trunk/zemberek-server/src/net/zemberekserver/server/Config.java
+++ trunk/zemberek-server/src/net/zemberekserver/server/Config.java
@@ -10,55 +10,58 @@ import java.util.Properties;
  */
 
 public class Config {
-    public static int serverPort;
-    public static boolean useDbus;
-    public static boolean useSockets;
-    public static boolean allowRemote;
-    public static String busName;
-    public static boolean useDbusSystemConnection;
-    
-    
-    private static Properties properties = null;
+	public static int serverPort;
+	public static boolean useDbus;
+	public static boolean useSockets;
+	public static boolean allowRemote;
+	public static String busName;
+	public static boolean useDbusSystemConnection;
 
-    static {
-        try {
-            properties = new Properties();
-            properties.load(new FileInputStream("config/conf.ini"));
-            serverPort = getPropertyValue("PORT_NUMBER", Defaults.PORT_NUMBER);
-            useDbus = getPropertyValue("USE_DBUS", Defaults.USE_DBUS);
-            useSockets = getPropertyValue("USE_SOCKETS", Defaults.USE_SOCKETS);
-            busName = getPropertyValue("BUS_NAME", Defaults.BUS_NAME);
-            allowRemote = getPropertyValue("ALLOW_REMOTE", Defaults.ALLOW_REMOTE);
-            useDbusSystemConnection = getPropertyValue("USE_DBUS_SYSTEM_CONNECTION", Defaults.USE_DBUS_SYSTEM_CONNECTION);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    private static String getPropertyValue(String propertyName, String defaultValue) {
-        String propertyValue = properties.getProperty(propertyName);
-        return propertyValue != null ? propertyValue.trim() : defaultValue;
-    }
+	private static Properties properties = null;
 
-    private static boolean getPropertyValue(String propertyName, boolean defaultValue) {
-        String propertyValue = properties.getProperty(propertyName);
-        return propertyValue != null ? propertyValue.trim().equalsIgnoreCase("YES") : defaultValue;
-    }
+	static {
+		try {
+			properties = new Properties();
+			String confFile = System.getProperty("ConfigFile");
+			if(confFile== null)
+				confFile=Defaults.CONFIG_FILE;
+			properties.load(new FileInputStream(confFile));
+			serverPort = getPropertyValue("PORT_NUMBER", Defaults.PORT_NUMBER);
+			useDbus = getPropertyValue("USE_DBUS", Defaults.USE_DBUS);
+			useSockets = getPropertyValue("USE_SOCKETS", Defaults.USE_SOCKETS);
+			busName = getPropertyValue("BUS_NAME", Defaults.BUS_NAME);
+			allowRemote = getPropertyValue("ALLOW_REMOTE", Defaults.ALLOW_REMOTE);
+			useDbusSystemConnection = getPropertyValue("USE_DBUS_SYSTEM_CONNECTION", Defaults.USE_DBUS_SYSTEM_CONNECTION);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    private static int getPropertyValue(String propertyName, int defaultValue) {
-        int val;
-        String propertyValue = properties.getProperty(propertyName);
-        if (propertyValue == null || propertyValue.equals("")) {
-            return defaultValue;
-        }
-        try {
-            val = Integer.parseInt(propertyValue.trim());
-        }
-        catch (NumberFormatException e) {
-            e.printStackTrace();
-            return defaultValue;
-        }
-        return val;
-    }
- } 
+	private static String getPropertyValue(String propertyName, String defaultValue) {
+		String propertyValue = properties.getProperty(propertyName);
+		return propertyValue != null ? propertyValue.trim() : defaultValue;
+	}
+
+	private static boolean getPropertyValue(String propertyName, boolean defaultValue) {
+		String propertyValue = properties.getProperty(propertyName);
+		return propertyValue != null ? propertyValue.trim().equalsIgnoreCase("YES") : defaultValue;
+	}
+
+	private static int getPropertyValue(String propertyName, int defaultValue) {
+		int val;
+		String propertyValue = properties.getProperty(propertyName);
+		if (propertyValue == null || propertyValue.equals("")) {
+			return defaultValue;
+		}
+		try {
+			val = Integer.parseInt(propertyValue.trim());
+		}
+		catch (NumberFormatException e) {
+			e.printStackTrace();
+			return defaultValue;
+		}
+		return val;
+	}
+} 
--- trunk/zemberek-server/src/net/zemberekserver/server/Defaults.java
+++ trunk/zemberek-server/src/net/zemberekserver/server/Defaults.java
@@ -1,6 +1,7 @@
 package net.zemberekserver.server;
 
 public class Defaults {
+	public static final String CONFIG_FILE="config/conf.ini";
 	public static final int PORT_NUMBER = 1234;
 	public static final String HOST_ADDRESS = "127.0.0.1";
 	public static final long DEFAULT_TIMEOUT = 3000; //ms
