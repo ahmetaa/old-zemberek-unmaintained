060ed345665ceaf1c4c7a0c22713166c1a7724d9
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-01-16 20:52:19 +0000

    * ZemberekDbusInterface.class ${dist.dir}/${isim}_dbus-${surum}.jar şeklinde ayri bir jar a tasindi (clientlar tum server jar ina bagimli olmamali) * Scriptler buna gore duzenlendi * Socket script ile client calistirilamiyor :(

--- trunk/zemberek-server/run.sh
+++ trunk/zemberek-server/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp lib/netty2.jar:lib/zemberek2-tr.jar:lib/dbus.jar:lib/unix.jar:lib/hexdump.jar -Djava.library.path=lib -Xverify:none -Xms12m -Xmx14m -jar zemberek_server-0.6.jar
+java -cp lib/zemberek2-tr.jar:lib/dbus.jar:lib/unix.jar:lib/hexdump.jar:lib/mina-core-1.1pre.jar:lib/slf4j-api-1.1.0.jar:lib/slf4j-nop-1.1.0.jar:zemberek_server_dbus-0.6.jar:zemberek_server-0.6.jar -Djava.library.path=lib -Xverify:none -Xms12m -Xmx14m net.zemberekserver.server.ZemberekServer
