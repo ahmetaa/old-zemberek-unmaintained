8a3a04077a3dca33afb4d1834f77b9d0d0d753e8
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-02-09 18:14:09 +0000

    Issue #35 - server

--- trunk/zemberek-server/src/net/zemberekserver/client/DBusClient.java
+++ trunk/zemberek-server/src/net/zemberekserver/client/DBusClient.java
@@ -1,9 +1,30 @@
-/************************************************
- *                                              *
- *  Zemberek DBus Arayüzü                       *
- *  @author Serkan Kaba <serkan_kaba@yahoo.com> *
- *                                              *
- ************************************************/
+/*
+ *  ***** BEGIN LICENSE BLOCK *****
+ *
+ *  Version: MPL 1.1
+ *
+ *  The contents of this file are subject to the Mozilla Public License Version
+ *  1.1 (the "License"); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.mozilla.org/MPL/
+ *
+ *  Software distributed under the License is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  The Original Code is "Zemberek Sunucu"
+ *
+ *  The Initial Developer of the Original Code is
+ *  Serkan Kaba.
+ *  Portions created by the Initial Developer are Copyright (C) 2006
+ *  the Initial Developer. All Rights Reserved.
+ *
+ *  Contributor(s):
+ *   Mehmet D. Akin
+ *
+ *  ***** END LICENSE BLOCK *****
+ */
 
 package net.zemberekserver.client;
 
