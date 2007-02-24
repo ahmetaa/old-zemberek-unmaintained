7735d8d4778d1b5c1549aca103dde059b1861bc6
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-02-24 03:20:03 +0000

    Jubler de kullandığım metin tabanlı kelime işleme metodu. İncelensin diye önce deney e ekledim

---
+++ zemberek2/deney/net/zemberek/deney/ZemberekDenetlemeAdayi.java
@@ -0,0 +1,58 @@
+package net.zemberek.deney;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import net.zemberek.erisim.Zemberek;
+import net.zemberek.tr.yapi.TurkiyeTurkcesi;
+import net.zemberek.yapi.DilAyarlari;
+
+public class ZemberekDenetlemeAdayi {
+
+	private Zemberek zemberek;
+
+	public ZemberekDenetlemeAdayi(DilAyarlari dilAyarlari) {
+		zemberek = new Zemberek(dilAyarlari);
+	}
+	
+	/* net.zemberek.erisim.Zemberek için aday metod */
+	public List<DenetlemeHatasi> metinDenetle(String text) {
+		Hashtable<String, Integer> sonPozisyonlar = new Hashtable<String, Integer>();
+		Vector<DenetlemeHatasi> ret = new Vector<DenetlemeHatasi>();
+		StringTokenizer tok = new StringTokenizer(text, "!'#%&/()=?-_:.,;\"\r\n\t ");
+		while (tok.hasMoreTokens()) {
+			String kelime = tok.nextToken();
+			int pos;
+			if (sonPozisyonlar.containsKey(kelime))
+				pos = text.indexOf(kelime, sonPozisyonlar.get(kelime) + kelime.length());
+			else
+				pos = text.indexOf(kelime);
+			sonPozisyonlar.put(kelime,pos);
+			if (!zemberek.kelimeDenetle(kelime))
+				ret.add(new DenetlemeHatasi(pos, kelime, zemberek.oner(kelime)));
+			sonPozisyonlar.put(kelime, pos);
+		}
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		ZemberekDenetlemeAdayi zemberek = new ZemberekDenetlemeAdayi(new TurkiyeTurkcesi());
+		List<DenetlemeHatasi> hatalar = zemberek.metinDenetle("yalnış yanlış doğru yanlız yalnız htalı hatalı değil");
+		for(DenetlemeHatasi hata: hatalar) {
+			System.out.println("Hatalı kelime: "+hata.getHatalikelime());
+			System.out.println("Offset: "+ hata.getHataPozisyonu());
+			System.out.print("Öneriler: ");
+			diziYazdir(hata.getOneriler());
+			System.out.println();
+			System.out.println("---------------------------");
+		}
+	}
+	
+	private static void diziYazdir(String[] array) {
+		for(String eleman: array)
+			System.out.print(eleman+" ");
+	}
+
+}
