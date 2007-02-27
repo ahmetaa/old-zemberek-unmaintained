b8340475de5b674c5485723e30c6df30ceccf631
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-02-27 04:34:10 +0000

    ZemberekFactory aday sınıfı ZemberekDenetlemeAdayi bunu kullaniyor

---
+++ trunk/zemberek2/deney/net/zemberek/deney/ZemberekFactory.java
@@ -0,0 +1,35 @@
+package net.zemberek.deney;
+
+import java.util.Hashtable;
+
+import net.zemberek.erisim.Zemberek;
+import net.zemberek.tm.yapi.Turkmence;
+import net.zemberek.tr.yapi.TurkiyeTurkcesi;
+
+public class ZemberekFactory {
+
+	private static Hashtable<String, Zemberek> zemberekNesneleri;
+	static {
+		zemberekNesneleri = new Hashtable<String, Zemberek>();
+	}
+
+	public static Zemberek getZemberek(String dilKodu) {
+		if (zemberekNesneleri.containsKey(dilKodu))
+			return zemberekNesneleri.get(dilKodu);
+		else {
+			Zemberek z = _getZemberek(dilKodu);
+			zemberekNesneleri.put(dilKodu, z);
+			return z;
+		}
+	}
+
+	private static Zemberek _getZemberek(String dilKodu) {
+		if (dilKodu.equalsIgnoreCase("tr"))
+			return new Zemberek(new TurkiyeTurkcesi());
+		else if (dilKodu.equalsIgnoreCase("tm"))
+			return new Zemberek(new Turkmence());
+		else
+			throw new IllegalArgumentException("Geçersiz dil kodu");
+	}
+
+}
