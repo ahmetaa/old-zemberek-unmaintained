2559d1e29a57c33cf60413c4cd55e2548fbed2fe
mdakin <mdakin@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-02-27 12:25:45 +0000

    Kitaplıkları ve başlatma betiklerini düzenle.

---
+++ zemberek-server/runtest_socket.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp .:zemberek_server-0.7.jar:lib/zemberek-cekirdek-2.0.jar:lib/zemberek-tr-2.0.jar:lib/zemberek-cekirdek-2.0.jar -Djava.library.path=lib net.zemberekserver.client.SocketClient
