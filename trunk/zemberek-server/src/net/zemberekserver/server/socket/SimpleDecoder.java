0419b8f696ed20b00db4a18d027fc504ebfcb73b
mdakin <mdakin@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-02-28 07:51:02 +0000

    Decoder kodunu güncelle

--- trunk/zemberek-server/src/net/zemberekserver/server/socket/SimpleDecoder.java
+++ trunk/zemberek-server/src/net/zemberekserver/server/socket/SimpleDecoder.java
@@ -45,50 +45,48 @@ public class SimpleDecoder implements ProtocolDecoder {
     private int okunacakMiktar;
     private int index = 0;
     
-	public void decode(IoSession session, ByteBuffer buffer,
-			ProtocolDecoderOutput out) throws Exception {
-		
-        // First read the message length. Length is binary coded decimal, like "12" , 
-		// one byte each character. We read while it is a number 
-        if (false == boyOkundu) {
-            while (buffer.hasRemaining()) {
-                byte read = buffer.get();
-                if (read >= '0' && read <= '9') {
-                    if (index > 7) {
-                    	throw new ProtocolDecoderException("Mesaj boyu çok büyük!. ");
-                    }
-                    temp[index++] = (byte)(read);
-                } else
-                    break;
-            }
-            // So , we have a legal message length info, convert it into an int, 
-            // reset counters, prepare a buffer.
-            if (index > 0) {
-                String boyString = new String(temp, 0, index);
-                mesajBoy = Integer.parseInt(boyString);
-                //System.out.println("Gelen stringin (UTF-8) boyu: " + mesajBoy);
-                boyOkundu = true;
-                kalan = mesajBoy;
-                index = 0;
-                govde = new byte[mesajBoy];
-            }
-        }
-        
-        if(kalan > 0){
-        	// Reading body.
-        	okunacakMiktar = kalan > buffer.remaining() ? buffer.remaining() : kalan;
-//        	System.out.println("Index: " + index + " Okunacak miktar: "
-//        			+ okunacakMiktar + " Bufferde Kalan: " + buffer.remaining()
-//        			+ " Kalan : " + kalan);
-        	buffer.get(govde, index, okunacakMiktar);
-        	index = index + okunacakMiktar;
-        	kalan = kalan - okunacakMiktar;
-        }
-		if (kalan == 0) {
-			out.write(govde);
-			reset();
-        }
-	}
+    public void decode(IoSession session, ByteBuffer buffer,
+    		ProtocolDecoderOutput out) throws Exception {
+    	while(buffer.hasRemaining()){
+    		// First read the message length. Length is ASCII coded decimal, like "12" , 
+    		// one byte each character. We read while it is a number 
+    		if (false == boyOkundu) {
+    			byte read = buffer.get();
+    			if (read >= '0' && read <= '9' && index<7) {
+    				temp[index++] = (byte)(read);
+    			}
+    			// So , we have a legal message length info, convert it into an int, 
+    			// reset counters, prepare a buffer.
+    			else{
+    				if(read != ' ' || index == 0 || index > 7) {
+    					throw new ProtocolDecoderException("Zemberek soket protokol hatası");
+    				}
+    				String boyString = new String(temp, 0, index);
+    				mesajBoy = Integer.parseInt(boyString);
+    				//System.out.println("Gelen stringin (UTF-8) boyu: " + mesajBoy);
+    				boyOkundu = true;
+    				kalan = mesajBoy;
+    				index = 0;
+    				govde = new byte[mesajBoy];
+    			}
+    		}
+
+    		if(kalan > 0){
+    			// Reading body.
+    			okunacakMiktar = kalan > buffer.remaining() ? buffer.remaining() : kalan;
+//  			System.out.println("Index: " + index + " Okunacak miktar: "
+//  			+ okunacakMiktar + " Bufferde Kalan: " + buffer.remaining()
+//  			+ " Kalan : " + kalan);
+    			buffer.get(govde, index, okunacakMiktar);
+    			index = index + okunacakMiktar;
+    			kalan = kalan - okunacakMiktar;
+    			if (kalan == 0) {
+    				out.write(govde);
+    				reset();
+    			}
+    		}
+    	}
+    }
 	
 	private void reset(){
 		boyOkundu = false;
