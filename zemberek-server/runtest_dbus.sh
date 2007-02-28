8a05d9ce41d75fc868cb61fbe680d37860903a81
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-02-28 19:58:22 +0000

    DBus düzeltmesi ------------------------- * Serverın kullandığı interface client ta direk kullanılamayabiliyor. Bu yüzden client a CreateInterface ile xml den oluşturulan arayüz eklendi.
    * Interface in ayrı jar olarak paketlenmesinden vazgeçildi (scriptler de buna göre düzenlendi)

--- zemberek-server/runtest_dbus.sh
+++ zemberek-server/runtest_dbus.sh
@@ -1,1 +1,1 @@
-java -cp .:lib/dbus.jar:lib/unix.jar:zemberek_server_dbus-0.7.jar:zemberek_server-0.7.jar -Djava.library.path=lib net.zemberekserver.client.DBusClient
+java -cp .:lib/dbus.jar:lib/unix.jar:zemberek_server-0.7.jar -Djava.library.path=lib net.zemberekserver.client.DBusClient
