e1fb59ac7a4ea2dc0878f7fd2e77a60d046f0ddc
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-02-28 19:58:22 +0000

    DBus düzeltmesi ------------------------- * Serverın kullandığı interface client ta direk kullanılamayabiliyor. Bu yüzden client a CreateInterface ile xml den oluşturulan arayüz eklendi.
    * Interface in ayrı jar olarak paketlenmesinden vazgeçildi (scriptler de buna göre düzenlendi)

--- trunk/zemberek-server/src/net/zemberekserver/client/DBusClient.java
+++ trunk/zemberek-server/src/net/zemberekserver/client/DBusClient.java
@@ -32,7 +32,7 @@ import org.freedesktop.dbus.DBusConnection;
 import org.freedesktop.dbus.exceptions.DBusException;
 
 import net.zemberekserver.server.Config;
-import net.zemberekserver.server.dbus.ZemberekDbusInterface;
+//import net.zemberekserver.server.dbus.ZemberekDbusInterface;
 
 import java.util.List;
 public class DBusClient {
@@ -54,13 +54,13 @@ public class DBusClient {
 		System.out.println("Sure: " + (System.currentTimeMillis() - start));
 		System.out.println(zemberek.kelimeDenetle("doğru"));
 		System.out.println(zemberek.kelimeDenetle("yalnış"));
-		String[] oneriler=zemberek.oner("yalnış");
+		List<String> oneriler=zemberek.oner("yalnış");
 		for ( String oneri: oneriler ) {
 			System.out.println(oneri);
 		}
 		
-		List<String[]> ayristirmalar = zemberek.kelimeAyristir("alayım");
-		for(String[] ayristirma: ayristirmalar) {
+		List<List<String>> ayristirmalar = zemberek.kelimeAyristir("alayım");
+		for(List<String> ayristirma: ayristirmalar) {
 			for(String parca: ayristirma) {
 				System.out.print(parca+" ");
 			}
