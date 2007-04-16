2c58ae9c82a0992485dfe964d947918328b9109c
mdakin <mdakin@c5a734b4-9d19-0410-97dd-ef9964c26567>
2007-04-16 10:32:17 +0000

    mina 1.1 e geç.

--- zemberek-server/run.sh
+++ zemberek-server/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp lib/zemberek-cekirdek-2.0.jar:lib/zemberek-tr-2.0.jar:lib/dbus.jar:lib/unix.jar:lib/mina-core-1.1pre.jar:lib/slf4j-nop.jar:zemberek_server-0.7.jar -Djava.library.path=lib -Xverify:none -Xms12m -Xmx14m net.zemberekserver.server.ZemberekServer
+java -cp lib/zemberek-cekirdek-2.0.jar:lib/zemberek-tr-2.0.jar:lib/dbus.jar:lib/unix.jar:lib/mina-core-1.1.jar:lib/slf4j-nop.jar:zemberek_server-0.7.jar -Djava.library.path=lib -Xverify:none -Xms12m -Xmx14m net.zemberekserver.server.ZemberekServer
--- zemberek-server/runtest_dbus.sh
+++ zemberek-server/runtest_dbus.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 java -cp .:lib/dbus.jar:lib/unix.jar:zemberek_server-0.7.jar -Djava.library.path=lib net.zemberekserver.client.DBusClient
