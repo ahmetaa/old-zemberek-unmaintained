7a2b0b385834abde93d145600409baa20cf566c2
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-04-06 16:59:41 +0000

    test scriptlerini düzenle 2

--- zemberek-server/runtest_socket.sh
+++ zemberek-server/runtest_socket.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-java -DUSE_DBUS_SYSTEM_CONNECTION=no \
-	-cp .:lib/zemberek-cekirdek-2.0.jar:lib/zemberek-tr-2.0.jar:lib/mina-core-1.1.0.jar:dist/zemberek_server-0.7.1.jar \
+java -cp .:lib/zemberek-cekirdek-2.0.jar:lib/zemberek-tr-2.0.jar:lib/mina-core-1.1.0.jar:dist/zemberek_server-0.7.1.jar \
 	-Djava.library.path=lib net.zemberekserver.client.SocketClient
