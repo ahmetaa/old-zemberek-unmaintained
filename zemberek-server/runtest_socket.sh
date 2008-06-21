dcbe210f3e7f95a8e0b624c686e298d9ea4559fb
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-06-21 10:56:12 +0000

    zemberek-2.1_rc1 etiketlendi.

--- zemberek-server/runtest_socket.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-java -cp .:lib/zemberek-cekirdek-2.0.jar:lib/zemberek-tr-2.0.jar:lib/mina-core-1.1.0.jar:dist/zemberek_server-0.7.1.jar \
-	-Djava.library.path=lib net.zemberekserver.client.SocketClient
