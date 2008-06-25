be1f77fa1168aebec12e821294bd28d6e6fe0974
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-06-25 20:48:04 +0000

    zemberek-2.1_rc1 jarları eklendi

--- zemberek-server/runtest_socket.sh
+++ zemberek-server/runtest_socket.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp .:lib/zemberek-cekirdek-2.0.jar:lib/zemberek-tr-2.0.jar:lib/mina-core-1.1.0.jar:dist/zemberek-server-0.7.1.jar \
+java -cp .:lib/zemberek-cekirdek-2.1_rc1.jar:lib/zemberek-tr-2.1_rc1.jar:lib/mina-core-1.1.0.jar:dist/zemberek-server-0.7.1.jar \
 	-Djava.library.path=lib/native-$(arch) net.zemberekserver.client.SocketClient
