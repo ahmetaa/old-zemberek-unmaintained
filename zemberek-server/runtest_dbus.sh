52db87319a842c88177c19782f7f6a6381605232
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-06-27 10:38:10 +0000

    sürümü 0.7.1_rc1 yap

--- zemberek-server/runtest_dbus.sh
+++ zemberek-server/runtest_dbus.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 java -DUSE_DBUS_SYSTEM_CONNECTION=no \
-	-cp .:lib/dbus.jar:lib/unix.jar:lib/hexdump.jar:zemberek-server-0.7.1.jar \
+	-cp .:lib/dbus.jar:lib/unix.jar:lib/hexdump.jar:zemberek-server-0.7.1_rc1.jar \
 	-Djava.library.path=lib/native-$(arch) net.zemberekserver.client.DBusClient
