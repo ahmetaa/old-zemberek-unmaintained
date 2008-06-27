7de254067fdeaee89bc952eb419ef8c139f19086
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-06-27 11:41:39 +0000

    zemberek-ooo-1.0_rc1 etiketle

--- zemberek-server/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-java -DUSE_DBUS_SYSTEM_CONNECTION=no \
-	-cp lib/zemberek-cekirdek-2.1_rc1.jar:lib/zemberek-tr-2.1_rc1.jar:lib/dbus.jar:lib/unix.jar:lib/hexdump.jar:lib/mina-core-1.1.0.jar:lib/slf4j-nop.jar:zemberek-server-0.7.1_rc1.jar \
-	-Djava.library.path=lib/native-$(arch) -Xverify:none -Xms12m -Xmx14m net.zemberekserver.server.ZemberekServer
