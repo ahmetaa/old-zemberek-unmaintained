fafe8b2361a8011c4a91bcede11d00cd81dff32f
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-07-10 07:16:25 +0000

    * zemberek-2.1 jarlarını ekle * Sürümü 0.7.1 yap

--- zemberek-server/runtest_dbus.sh
+++ zemberek-server/runtest_dbus.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 java -DUSE_DBUS_SYSTEM_CONNECTION=no \
-	-cp zemberek-server-0.7.1_rc1.jar \
+	-cp zemberek-server-0.7.1.jar \
 	-Djava.library.path=lib/native-$(arch) net.zemberekserver.client.DBusClient
