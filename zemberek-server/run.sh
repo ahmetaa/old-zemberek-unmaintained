c5ae7c10c2f763f318fe627f9c83762b325369e3
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-07-10 07:09:11 +0000

    zemberek-2.1 etiketlendi

--- zemberek-server/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-java -DUSE_DBUS_SYSTEM_CONNECTION=no \
-	-cp zemberek-server-0.7.1_rc1.jar \
-	-Djava.library.path=lib/native-$(arch) -Xverify:none -Xms12m -Xmx14m net.zemberekserver.server.ZemberekServer
--- zemberek-server/runtest_socket.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-java -cp zemberek-server-0.7.1_rc1.jar \
-	-Djava.library.path=lib/native-$(arch) net.zemberekserver.client.SocketClient
