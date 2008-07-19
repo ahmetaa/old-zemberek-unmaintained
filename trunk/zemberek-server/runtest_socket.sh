9bc72a961d2eabf3865eac21e0f6ec54944bb140
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-07-19 21:21:39 +0000

    FreeBSD 7 için jni kütüphanesi eklendi.

--- trunk/zemberek-server/runtest_socket.sh
+++ trunk/zemberek-server/runtest_socket.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source runbase.sh
 java -cp zemberek-server-${surum}.jar \
-	-Djava.library.path=lib/native-$(uname -m) net.zemberekserver.client.SocketClient
+	-Djava.library.path=lib/$(get_jni_dir) net.zemberekserver.client.SocketClient
