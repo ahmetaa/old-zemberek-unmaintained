347854a212b52cc0779b73b28ff2f867963133df
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-07-19 21:21:39 +0000

    FreeBSD 7 için jni kütüphanesi eklendi.

--- zemberek-server/run.sh
+++ zemberek-server/run.sh
@@ -2,4 +2,4 @@
 source runbase.sh
 java -DUSE_DBUS_SYSTEM_CONNECTION=no \
 	-cp zemberek-server-${surum}.jar \
-	-Djava.library.path=lib/native-$(uname -m) -Xverify:none -Xms12m -Xmx14m net.zemberekserver.server.ZemberekServer
+	-Djava.library.path=lib/$(get_jni_dir) -Xverify:none -Xms12m -Xmx14m net.zemberekserver.server.ZemberekServer
