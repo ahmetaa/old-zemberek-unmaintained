91c1f28d8435b541194f533c03298df680412fa7
serkankaba <serkankaba@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-08-29 07:41:13 +0000

    SocketClient JNI kütüphanesine ihtiyaç duymuyor

--- zemberek-server/runtest_socket.sh
+++ zemberek-server/runtest_socket.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source runbase.sh
 java -cp zemberek-server-${surum}.jar \
-	-Djava.library.path=lib/$(get_jni_dir) net.zemberekserver.client.SocketClient
+	net.zemberekserver.client.SocketClient
