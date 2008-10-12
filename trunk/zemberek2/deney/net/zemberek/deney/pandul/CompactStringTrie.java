c57e174f7bb25d71dd6c186db07b3bff3118961c
mdakin <mdakin@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-10-12 23:22:52 +0000

    Still nothing fancy, a little more stuff. very simple tree generation, a few more fixes. Changed Latters to TurkishAlphabet, but that will change as well.

--- trunk/zemberek2/deney/net/zemberek/deney/pandul/CompactStringTrie.java
+++ trunk/zemberek2/deney/net/zemberek/deney/pandul/CompactStringTrie.java
@@ -3,25 +3,40 @@ package net.zemberek.deney.pandul;
 import java.util.List;
 
 public class CompactStringTrie {
-  private Node root;
- 
+  private Node root = new Node();
+
   public void add(String s) {
-    char[] giris = s.toCharArray();
+    char[] chars = s.toCharArray();
     Node node = root;
     Node previousNode = null;
-    int idx = 0;
+    int i = 0;
     // null alt düğüm bulana dek veya kelimenin sonuna gelene
     // dek kelimenin harflerini izleyerek alt düğümlerde ilerle
-    while (idx < giris.length) {
+    while (i < chars.length) {
       previousNode = node;
-      node = node.getChildNode(giris[idx]);
-      if (node == null) break;
-      idx++;
+      node = node.getChildNode(chars[i]);
+      if (node == null)
+        break;
+      i++;
     }
+
+    if (i < chars.length) {
+      // Start from the parent.
+      node = previousNode;
+      while (i < chars.length) {
+        node = node.addNodeFor(chars[i++]);
+      }
+      node.setAttribute(1);
+    }
+
   }
-  
+
   public List<String> get(String prefix) {
     return null;
   }
-   
+
+  public Node getRoot() {
+    return root;
+  }
+
 }
--- trunk/zemberek2/test/deney/net/zemberek/deney/pandul/PandulTest.java
+++ trunk/zemberek2/test/deney/net/zemberek/deney/pandul/PandulTest.java
@@ -5,22 +5,22 @@ import junit.framework.TestCase;
 public class PandulTest extends TestCase {
 
   public void testCharToIndexForIllegalChars(){
-    assertEquals(-1, Letters.getIndex('+'));
-    assertEquals(-1, Letters.getIndex('?'));
-    assertEquals(-1, Letters.getIndex('q'));
-    assertEquals(-1, Letters.getIndex(' '));
+    assertEquals(-1, TurkishAlphabet.getIndex('+'));
+    assertEquals(-1, TurkishAlphabet.getIndex('?'));
+    assertEquals(-1, TurkishAlphabet.getIndex('q'));
+    assertEquals(-1, TurkishAlphabet.getIndex(' '));
   }
 
  public void testCharToIndex(){
-   assertEquals(0, Letters.getIndex('a'));
-   assertEquals(1, Letters.getIndex('b'));
-   assertEquals(28, Letters.getIndex('z'));
+   assertEquals(0, TurkishAlphabet.getIndex('a'));
+   assertEquals(1, TurkishAlphabet.getIndex('b'));
+   assertEquals(28, TurkishAlphabet.getIndex('z'));
  }
 
  public void testIndexToChar() {
-   assertEquals('a', Letters.getChar(0));
-   assertEquals('b', Letters.getChar(1));
-   assertEquals('z', Letters.getChar(28));
+   assertEquals('a', TurkishAlphabet.getChar(0));
+   assertEquals('b', TurkishAlphabet.getChar(1));
+   assertEquals('z', TurkishAlphabet.getChar(28));
  }
  
 }
