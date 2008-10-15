bca4d7ad1f5b1b12b22578cec94b13c6e5b9215f
ahmetaa <ahmetaa@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-10-15 18:11:22 +0000

    6 bit char, 4 bit length version.

--- zemberek2/deney/net/zemberek/deney/pandul/TinyStrings.java
+++ zemberek2/deney/net/zemberek/deney/pandul/TinyStrings.java
@@ -1,27 +1,34 @@
 package net.zemberek.deney.pandul;
 
 public class TinyStrings {
+
+  private static final int LENGTH_BIT_MASK = 0x0F;
+  private static final int MAX_STRING_LEGTH = 10;
+  private static final int CHAR_BIT_SIZE = 6;
+  private static final int CHAR_BIT_MASK = 0x3F;
+  private static final int LENGTH_BIT_SIZE = 4;
+
   /**
    * long based constructor. checks if input parameter is valid.
    *
    * @param value the long
    * @return long value.
-   * @throws IndexOutOfBoundsException if String size is not between [0-7] (inclusive)
+   * @throws IndexOutOfBoundsException if String size is not between [0-10] (inclusive)
    * @throws IllegalArgumentException  if the input vlaue is not valid. (size and idex values)
    */
   public static long create(long value) {
     final int l = length(value);
-    if (l < 0 || l > 7)
-      throw new IndexOutOfBoundsException("length must be between [0..7]. But it is:" + l);
+    if (l < 0 || l > 9)
+      throw new IndexOutOfBoundsException("length must be between [0.." + (MAX_STRING_LEGTH - 1) + "]. But it is:" + l);
 
-    long s = value >> 8;
+    long s = value >> LENGTH_BIT_SIZE;
     for (int i = 0; i < l; i++) {
-      long t = s & 0xff;
+      long t = s & CHAR_BIT_MASK;
       if (t < 0 || t >= TurkishAlphabet.ALPHABET_SIZE)
         throw new IllegalArgumentException("String connot contain characters outside TurkishAlphabet");
-      s = s >> 8;
+      s = s >> CHAR_BIT_SIZE;
     }
-    return s;
+    return value;
   }
 
   /**
@@ -36,16 +43,18 @@ public class TinyStrings {
     if (str == null || str.length() == 0) {
       return 0L;
     }
-    if (str.length() > 7) {
-      throw new IndexOutOfBoundsException("Compact String size cannot be larger than 7");
+    if (str.length() > MAX_STRING_LEGTH) {
+      throw new IndexOutOfBoundsException("String size cannot be larger than " + MAX_STRING_LEGTH);
     }
     long t = 0;
     for (int i = str.length() - 1; i >= 0; i--) {
-      char c = str.charAt(i);
-      int index = TurkishAlphabet.getIndex(c);
+      int index = TurkishAlphabet.getIndex(str.charAt(i));
       if (index == -1)
         throw new IllegalArgumentException("String connot contain characters outside TurkishAlphabet");
-      t = (t | index) << 8;
+      if (i > 0)
+        t = (t | index) << CHAR_BIT_SIZE;
+      else
+        t = (t | index) << LENGTH_BIT_SIZE;
     }
     t = t | str.length();
     return t;
@@ -61,7 +70,7 @@ public class TinyStrings {
   public static long create(char c) {
     final int index = TurkishAlphabet.getIndex(c);
     if (index != -1)
-      return index << 8 | 0x01;
+      return index << CHAR_BIT_SIZE | 0x01;
     else
       throw new IllegalArgumentException("char:" + c + "cannot be outside TurkishAlphabet");
   }
@@ -74,20 +83,21 @@ public class TinyStrings {
    * @return length
    */
   public static int length(long value) {
-    return (int) value & 0xFF;
+    return (int) value & LENGTH_BIT_MASK;
   }
 
   /**
    * returns the char at given index. index value must be between [0..length) length exclusive.
    *
-   * @param l the long representation.
+   * @param l     the long representation.
    * @param index index
    * @return char at that index.
    */
-  public char charAt(long l, int index) {
+  public static char charAt(long l, int index) {
     if (index < 0 || index >= length(l))
       throw new IndexOutOfBoundsException("index must be between [0.." + length(l) + "). But it is:" + index);
-    return TurkishAlphabet.getChar((int) ((l >> (index + 1) * 8) & 0xff));
+    l = l >> LENGTH_BIT_SIZE;
+    return TurkishAlphabet.getChar((int) ((l >> ( index  * CHAR_BIT_SIZE)) & CHAR_BIT_MASK));
   }
 
   /**
@@ -102,10 +112,10 @@ public class TinyStrings {
       return "";
 
     StringBuilder sb = new StringBuilder(length);
-    l = l >> 8;
+    l = l >> LENGTH_BIT_SIZE;
     for (int i = 0; i < length; i++) {
-      sb.append(TurkishAlphabet.getChar((int) l & 0xff));
-      l = l >> 8;
+      sb.append(TurkishAlphabet.getChar((int) l & CHAR_BIT_MASK));
+      l = l >> CHAR_BIT_SIZE;
     }
     return sb.toString();
   }
