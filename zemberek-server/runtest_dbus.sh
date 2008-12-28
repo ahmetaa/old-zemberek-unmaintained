21666cc07df91dda9c71fc5f3b87e86507c557a0
railaliev <railaliev@c5a734b4-9d19-0410-97dd-ef9964c26567>
2008-12-28 17:44:15 +0000

    0.0.2 is out

--- zemberek-server/runtest_dbus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-source runbase.sh
-java -DUSE_DBUS_SYSTEM_CONNECTION=no \
-	-cp zemberek-server-${surum}.jar \
-	-Djava.library.path=lib/$(get_jni_dir) net.zemberekserver.client.DBusClient
