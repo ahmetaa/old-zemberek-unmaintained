5e15fa6417aa2cf0d8b911115915392fa320a41b
mdakin <mdakin@c5a734b4-9d19-0410-97dd-ef9964c26567>
2009-07-02 15:18:17 +0000

    Some more experiments, get rid of long hack, start measuring more blah, start FlatTrie implementation. All crap for now..

---
+++ zemberek2/test/deney/net/zemberek/deney/pandul/SimpleCompactStringTrieTest.java
@@ -0,0 +1,165 @@
+package net.zemberek.deney.pandul;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+
+public class SimpleCompactStringTrieTest extends TestCase {
+  TurkishAlphabet alphabet = new TurkishAlphabet();
+  public void testSingleChar() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("a");
+    System.out.println(cst.getRoot());
+  }
+
+  public void testTwoStrings() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("a");
+    cst.add("b");
+    System.out.println(cst.getRoot());
+  }
+
+  public void testSingleWord() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("a");
+    assertEquals ("#:(a)|a:.*|" , cst.getRoot().dump(true));
+    cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("a");
+    cst.add("b");
+    assertEquals ("#:(ab)|a:.*|b:.*|" , cst.getRoot().dump(true));
+  }
+  
+  public void testLongString() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("a");
+    cst.add("ab");
+    cst.add("c");
+    assertEquals ("#:(ac)|a:(b)*|b:.*|c:.*|" , cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }
+
+  public void testLongString2() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("ela");
+    cst.add("elma");
+    cst.add("elmas");
+    cst.add("ek");
+    cst.add("et");
+    assertEquals ("#:(e)|e:(klt)|k:.*|l:(am)|a:.*|m:(a)|a:(s)*|s:.*|t:.*|", cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }
+
+  public void testCompactionDoesNotEffectEmptyTrie() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.compress();
+    assertEquals("#:.|", cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }
+  
+  public void testCompactionDoesNotEffectSingleCharTrie() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("a");
+    cst.compress();
+    assertEquals("#:(a)|a:.*|", cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }  
+  
+  public void testCompactionDoesNotEffectUncompactableTrie() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("a");
+    cst.add("ab");
+    cst.add("c");
+    cst.compress();
+    assertEquals ("#:(ac)|a:(b)*|b:.*|c:.*|" , cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }
+  
+  public void testCompactionSimpleCase() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("ab");
+    System.out.println(cst.getRoot().dump(false));
+    cst.compress();
+    assertEquals ("#:(a)|ab:.*|" , cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  } 
+  
+  public void testCompactionSimpleCase2() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("patlak");
+    System.out.println(cst.getRoot().dump(false));
+    cst.compress();
+    assertEquals ("#:(p)|patlak:.*|" , cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }
+  
+  public void testCompactionLongChain() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("cobansumbulu");
+    System.out.println(cst.getRoot().dump(false));
+    cst.compress();
+    assertEquals ("#:(c)|cobansumbulu:.*|" , cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }  
+  
+  public void testCompactionSimpleCase3() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("abc");
+    cst.add("vyz");
+    cst.compress();
+    assertEquals ("#:(av)|abc:.*|vyz:.*|" , cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }    
+  
+  public void testCompactionTwoChains() {
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("ela");
+    cst.add("elmas");
+    System.out.println(cst.getRoot().dump(false));
+    cst.compress();
+    assertEquals ("#:(e)|el:(am)|a:.*|mas:.*|" , cst.getRoot().dump(true));
+    System.out.println(cst.getRoot().dump(false));
+  }   
+  
+  public void testSave() throws IOException {
+    ByteArrayOutputStream bo = new ByteArrayOutputStream();
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("a");
+    cst.add("b");
+    cst.add("elma");
+    cst.compress();
+    String s1 = cst.getRoot().dump(true);
+    cst.save(new BufferedOutputStream(bo));
+    byte[] b = bo.toByteArray();
+    assertNotNull(b);
+    assertTrue(b.length > 0);
+    System.out.println(b.length);
+    ByteArrayInputStream bi = new ByteArrayInputStream(b);
+    SimpleCompactStringTrie cst2 = new SimpleCompactStringTrie(alphabet);
+    cst2.load(new BufferedInputStream(bi));
+    String s2 = cst.getRoot().dump(true);
+    assertEquals(s1, s2);
+  }
+  
+  public void testFindPrefixMatches(){
+    SimpleCompactStringTrie cst = new SimpleCompactStringTrie(alphabet);
+    cst.add("el");
+    cst.add("elma");
+    cst.add("ela");
+    cst.add("elmas");
+    cst.add("et");
+    cst.compress();
+    System.out.println(cst.getRoot().dump(false));
+    List<String> roots = cst.getMatchingRoots("elmaslara");
+    assertEquals(3, roots.size());
+    assertEquals("el", roots.get(0));
+    assertEquals("elma", roots.get(1));
+    assertEquals("elmas", roots.get(2));
+  }
+  
+}
